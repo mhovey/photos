c57adcf39705e94392664c3f3aaaf94d83e92d60
Michael Hovey <mhovey@getsmartcontent.com>
2015-08-07 15:29:18 -0500

    Implement restify for controller endpoints

--- server.js
+++ server.js
@@ -4,34 +4,48 @@
 |--------------------------------------------------------------------------
 */
 
-var express    = require('express');
-var mongoose   = require('mongoose');
-var bodyParser = require('body-parser');
-var morgan     = require('morgan');
-var app        = express();
-var router     = express.Router();
-var port       = process.env.PORT || 3000;
+var express        = require('express');
+var mongoose       = require('mongoose');
+var bodyParser     = require('body-parser');
+var morgan         = require('morgan');
+var _              = require('underscore');
+var methodOverride = require('method-override');
+var Schema         = mongoose.Schema;
+var app            = express();
+var router         = express.Router();
+var port           = process.env.PORT || 3000;
+var restify        = require('express-restify-mongoose');
+var apiVersion     = '/v1';
+
+/*
+|--------------------------------------------------------------------------
+| Import Models
+|--------------------------------------------------------------------------
+*/
+
+var Photo = require('./models/photo');
 
 /*
 |--------------------------------------------------------------------------
 | Database Connection with Mongoose
 |--------------------------------------------------------------------------
 */
+
 mongoose.connect('mongodb://localhost/hoveyfamilyphotos');
 
 /*
 |--------------------------------------------------------------------------
-| Various Middleware
+| Configure the App
 |--------------------------------------------------------------------------
 */
 
 // BodyParser for handling JSON bodies in POST requests
-app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(methodOverride());
 
 // Morgan to log requests to the console
 app.use(morgan('dev'));
-
 
 /*
 |--------------------------------------------------------------------------
@@ -40,15 +54,21 @@ app.use(morgan('dev'));
 */
 
 // Base route
-router.get('/', function(req, res) {
+router.get('/'+apiVersion, function(req, res) {
   res.json({ message: 'Good stuff! You have reached the base endpoint for the API.' });
 });
 
-// Model-based routes stored in controllers
-app.use('/photos', require('./controllers/photos'));
+// Define Restify options
+var restifyOptions = {
+  prefix: '',
+  version: apiVersion,
+  lowercase: true,
+  plural: true
+};
 
-// Register routes
-app.use('/', router);
+// Set up routes for models with restify
+restify.serve(router, Photo, restifyOptions);
+app.use(router);
 
 /*
 |--------------------------------------------------------------------------
