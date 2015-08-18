5bfc23fc1e6582b9bb003b6e24a24090c89cb9ea
Michael Hovey <mhovey@getsmartcontent.com>
2015-08-18 10:14:29 -0500

    Refine user schema and include bcrypt encryption for storing the user's password

--- models/user.js
+++ models/user.js
@@ -5,7 +5,8 @@
 */
 
 var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
+var bcrypt   = require('bcrypt');
+var Schema   = mongoose.Schema;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,10 +15,10 @@ var Schema = mongoose.Schema;
 */
 
 var userSchema = new Schema({
-  name: String,
-  username: String,
-  email: String,
-  password: String,
+  name: { type: String, required: true },
+  username: { type: String, required: true, index: { unique: true } },
+  email: { type: String, required: true, index: { unique: true } },
+  password: { type: String, required: true },
   meta: Object,
   created_at: {type: Date, default: Date.now},
   updated_at: {type: Date, default: Date.now}
@@ -25,10 +26,60 @@ var userSchema = new Schema({
 
 /*
 |--------------------------------------------------------------------------
+| Middleware for the User Model
+|--------------------------------------------------------------------------
+*/
+
+// Alter User entries as they are saved
+userSchema.pre('save', function(next) {
+
+  var user = this;
+
+  // Only hash the password if it has been modified (or is new)
+  if(!user.isModified('password')) return next();
+
+  // Generate a salt
+  bcrypt.genSalt(10, function(err, salt) {
+
+    if(err) return next(err);
+
+    // Hash the password using our new salt
+    bcrypt.hash(user.password, salt, function(err, hash) {
+
+      if(err) return next(err);
+
+      // Override the cleartext password with the hashed one
+      user.password = hash;
+      next();
+
+    });
+
+  });
+
+});
+
+/*
+|--------------------------------------------------------------------------
+| Define custom methods for the User Schema
+|--------------------------------------------------------------------------
+*/
+
+// Define a method for comparing the provided password with the salted version
+userSchema.methods.comparePassword = function(candidatePassword, cb) {
+
+  bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
+
+    if(err) return cb(err);
+    cb(null, isMatch);
+
+  });
+
+};
+
+/*
+|--------------------------------------------------------------------------
 | Assign the schema and export
 |--------------------------------------------------------------------------
 */
 
-var User = mongoose.model('User', userSchema);
-
-module.exports = User;
+module.exports = mongoose.model('User', userSchema);
