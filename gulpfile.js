ff814e1f37ee8de30d310966e1a268e39d776518
Michael Hovey <mhovey@getsmartcontent.com>
2015-08-22 07:30:19 -0500

    Begin setting up gulp

---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+var gulp = require('gulp');
+
+gulp.task('default', ['browser-sync'], function () {
+});
+
+gulp.task('browser-sync', ['nodemon'], function() {
+    browserSync.init(null, {
+        proxy: "http://localhost:3000",
+        files: ["public/**/*.*"],
+        browser: "google chrome",
+        port: 7000,
+    });
+});
+
+gulp.task('nodemon', function (cb) {
+
+  var started = false;
+
+  return nodemon({
+    script: 'app.js'
+  }).on('start', function () {
+    // to avoid nodemon being started multiple times
+    if (!started) {
+        cb();
+        started = true;
+    }
+  });
+
+});
