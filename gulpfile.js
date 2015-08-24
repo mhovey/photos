9c43b865133b5fe56827e849b645b547b333e5ec
Michael Hovey <mhovey@getsmartcontent.com>
2015-08-23 23:32:37 -0500

    Set up Gulp with SCSS compiling

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,60 @@
 var gulp = require('gulp');
+var concat = require("gulp-concat");
+var minifyCSS = require("gulp-minify-css");
+var rename = require("gulp-rename");
+var sass = require("gulp-sass");
+var uglify = require("gulp-uglify");
+var sourcemaps = require("gulp-sourcemaps");
+var notify = require("gulp-notify");
+var watch = require("gulp-watch");
 
-gulp.task('default', ['browser-sync'], function () {
+function compileSass(includeSourceMaps) {
+  console.log("*.scss file change detected, compiling and minifying sass into main.css...");
+
+  if (includeSourceMaps) {
+    gulp.src("./public/scss/main.scss")
+      .pipe(sourcemaps.init())
+        .pipe(sass())
+        .pipe(minifyCSS())
+      .pipe(sourcemaps.write())
+      .pipe(gulp.dest("./public/css"))
+      .pipe(notify({
+        title: "Gulp",
+        message: "SCSS Compiled with Sourcemaps"
+      }));
+  } else {
+    gulp.src("./public/scss/main.scss")
+      .pipe(sass())
+      .pipe(minifyCSS())
+      .pipe(gulp.dest("./public/css"))
+      .pipe(notify({
+        title: "Gulp",
+        message: "SCSS Compiled"
+      }));
+  }
+}
+
+gulp.task("watch-sass-debug", function() {
+  watchSass(true);
 });
 
-gulp.task('browser-sync', ['nodemon'], function() {
-    browserSync.init(null, {
-        proxy: "http://localhost:3000",
-        files: ["public/**/*.*"],
-        browser: "google chrome",
-        port: 7000,
-    });
+gulp.task("compile-sass-and-minify-css", function() {
+  compileSass();
 });
 
-gulp.task('nodemon', function (cb) {
+/*
+ * Default Gulp task:
+ * runs all specified tasks when you run "gulp" command
+ */
+gulp.task("default", [
+  "compile-sass-and-minify-css"
+]);
 
-  var started = false;
-
-  return nodemon({
-    script: 'app.js'
-  }).on('start', function () {
-    // to avoid nodemon being started multiple times
-    if (!started) {
-        cb();
-        started = true;
-    }
+function watchSass(includeSourceMaps) {
+  watch([
+    "./public/scss/main.scss",
+    "./public/scss/**/*.scss"
+  ], function() {
+    compileSass(includeSourceMaps);
   });
-
-});
+}
